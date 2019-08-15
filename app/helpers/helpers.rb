aaf05e2c64b7df5f33772179a4ba2e96291f53d2
Brian Simonton <bsimonton78@gmail.com>
2019-08-15 17:20:53 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,7 +3,7 @@ class Helpers
   
   
   
- def current_user(session) 
+ def current_user(session_hash) 
    
  end 
  
@@ -11,7 +11,7 @@ class Helpers
  
  
  
- def is_logged_in? 
+ def is_logged_in?(session_hash) 
    
  end 
   
